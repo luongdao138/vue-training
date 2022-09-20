4b68e91cc364feb061145506b6aca6fd3a4072ec
LuongDV85025 <luongdv85025@co-well.vn>
2022-09-20 13:46:13 +0700

    init

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,4 @@
-<script setup>
-import { reactive } from "vue";
-import { useRouter } from "vue-router";
+<script lang="ts">
 import { mdiAccount, mdiAsterisk } from "@mdi/js";
 import SectionFullScreen from "@/components/SectionFullScreen.vue";
 import CardBox from "@/components/CardBox.vue";
@@ -11,35 +9,51 @@ import BaseButton from "@/components/BaseButton.vue";
 import BaseButtons from "@/components/BaseButtons.vue";
 import LayoutGuest from "@/layouts/LayoutGuest.vue";
 
-const form = reactive({
-  login: "john.doe",
-  pass: "highly-secure-password-fYjUw-",
-  remember: true,
-});
-
-const router = useRouter();
-
-const submit = () => {
-  router.push("/dashboard");
+export default {
+  components: {
+    SectionFullScreen,
+    CardBox,
+    FormCheckRadio,
+    FormField,
+    FormControl,
+    BaseButton,
+    BaseButtons,
+    LayoutGuest,
+  },
+  data: () => {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+      mdiAccount,
+      mdiAsterisk,
+    };
+  },
+  methods: {
+    handleLogin() {
+      console.log(this.form);
+    },
+  },
 };
 </script>
 
 <template>
   <LayoutGuest>
     <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
-      <CardBox :class="cardClass" is-form @submit.prevent="submit">
-        <FormField label="Login" help="Please enter your login">
+      <CardBox :class="cardClass" is-form @submit.prevent="handleLogin">
+        <FormField label="Username" help="Please enter your username">
           <FormControl
-            v-model="form.login"
+            v-model="form.username"
             :icon="mdiAccount"
-            name="login"
+            name="username"
             autocomplete="username"
           />
         </FormField>
 
         <FormField label="Password" help="Please enter your password">
           <FormControl
-            v-model="form.pass"
+            v-model="form.password"
             :icon="mdiAsterisk"
             type="password"
             name="password"
